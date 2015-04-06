efc1240f1de9cf108c9abd28eaa23479bbd4ef75
Adam Smith <adamsmith910@aol.com>
2015-04-06 11:15:21 -0600

    everything done except getting the app to actually work, tests written and necessary folders made

---
+++ spec/controllers/api/v1/commute_time/data_controller_spec.rb
@@ -0,0 +1,16 @@
+require "rails_helper"
+
+RSpec.describe Api::V1::CommuteTime::DataController, type: :controller do
+  describe 'get index' do
+    it "returns list of all data as json" do
+      create(:state_commute_time_data)
+      get :index
+      data = JSON.parse(response.body)
+      expect(response).to have_http_status(:success)
+      expect(data.first).to be_a(Hash)
+      expect(data.first["year"]).to eq(2010)
+      expect(data.first[""]).to eq("")
+      expect(data.first["state"]).to eq("CO")
+    end
+  end
+end
