51ee49061c586e859ee3e1bac28e7685ddb91941
Adam Smith <adamsmith910@aol.com>
2015-04-07 13:04:59 -0600

    factory for state marital data and data controller test for state marital data passing

---
+++ spec/controllers/api/v1/marital/data_controller_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe Api::V1::Marital::DataController, type: :controller do
+  describe 'get index' do
+    it "returns list of all data as json" do
+      create(:state_marital_data)
+      get :index
+      data = JSON.parse(response.body)
+      expect(response).to have_http_status(:success)
+      expect(data.first).to be_a(Hash)
+      expect(data.first["year"]).to eq(2010)
+      expect(data.first["male_population"]).to eq(6000000)
+      expect(data.first["state"]).to eq("CO")
+    end
+  end
+end
