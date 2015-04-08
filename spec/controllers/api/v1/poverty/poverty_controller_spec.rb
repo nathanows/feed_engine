f7848fdf803c2fa33dbcd26e90450dc9d901a9be
Alex Robinson <alexandra.scott335@gmail.com>
2015-04-07 18:47:41 -0600

    added general poverty data serializer

--- spec/controllers/api/v1/poverty/poverty_controller_spec.rb
+++ spec/controllers/api/v1/poverty/poverty_controller_spec.rb
@@ -9,7 +9,8 @@ RSpec.describe Api::V1::Poverty::PovertyController, type: :controller do
       data = JSON.parse(response.body)["poverty"]
 
       expect(response).to have_http_status(:success)
-      expect(data.first["years_available"]).to eq("Data available for the following year(s): 2010")
+      expect(data.first["years_available"]).
+        to eq("Data available for the following year(s): [2010]")
     end
   end
 end
