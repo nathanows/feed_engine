564e44f5b17f57e91b405218b3b96828d1bedeca
Adam Smith <adamsmith910@aol.com>
2015-04-16 10:13:05 -0600

    fixing database problems

--- app/models/state_marital_data.rb
+++ app/models/state_marital_data.rb
@@ -1,13 +1,10 @@
 class StateMaritalData < ActiveRecord::Base
 
   def never_married_percentage
-    (self.male_never_married + self.female_never_married) / self.population.to_f * 100
+    ((self.male_never_married + self.female_never_married) / self.population.to_f) * 100
   end
 
   def ever_married_percentage
-    (self.male_ever_married + self.male_ever_married_and_married_last_year +
-    self.male_ever_married_and_not_married_last_year + self.female_ever_married
-    + self.female_ever_married_and_married_last_year +
-    self.female_ever_married_and_not_married_last_year) / self.population.to_f * 100
+    ((self.male_ever_married + self.female_ever_married) / self.population.to_f) * 100
   end
 end
